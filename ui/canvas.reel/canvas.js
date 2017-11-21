48f9cb3e2e3e30b7613dbc45268d4308aea6d379
onormal <noreply>
2017-11-21 14:54:40 +0000

    Add component canvas

---
+++ ui/canvas.reel/canvas.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/canvas.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Canvas
+ * @extends Component
+ */
+exports.Canvas = Component.specialize(/** @lends Canvas# */ {
+    constructor: {
+        value: function Canvas() {
+            this.super();
+        }
+    }
+});
